7acda59cd31e20d68a6b3710a0c25ed086953c85
Yourself77777 <lindon.corbin@ucas-edu.net>
2025-09-02 14:35:36 -0600

    I finished variable practice

---
+++ practices/variables_practices.py
@@ -0,0 +1,23 @@
+#LC Per 7 variables practices
+
+name = "Lindon"
+number = "3"
+number_more = "300"
+breakfast = "hashbrown"
+color = "orange"
+school = "UCAS"
+year = "2025"
+eye_color = "brown"
+age = "14"
+subject = "chemistry"
+
+print(name)
+print(number)
+print(number_more)
+print(breakfast)
+print(color)
+print(school)
+print(year)
+print(eye_color)
+print(age)
+print(subject)
