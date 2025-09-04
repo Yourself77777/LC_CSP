ff5347b1214c9345840b4978668d34f6e73e81eb
Yourself77777 <lindon.corbin@ucas-edu.net>
2025-09-04 14:41:30 -0600

    expression_note

---
+++ Notes/expressions_notes.py
@@ -0,0 +1,47 @@
+# LC 7th expression_notes
+
+# What is an algorithm-
+# A set of instructions to complete a task
+#example 
+name = input("yo whats is your name:\n")
+print ("hello", name)
+#example 
+# Treyson has 12 apples, he has 5 friends that he wants to give apples to.
+#how many apples does each friend get?
+apples = 12
+friends = 5
+print("eachfriend gets", apples//friends, "apples !")
+
+#List steps in an algorithm-
+# step one- variables (get needed info)
+# step two- do the equations (do the things)
+# step thre- display results
+#example 
+# Average age of a group of 4 people
+friend1 = 12 
+friend2 = 15
+friend3 = 18
+friend4 = 16
+all_together = friend1 + friend2 + friend3 + friend4
+average = (all_together // 4)
+print("average age of every friend", average)
+
+#List ALL of the different mathematical operators (Give me the symbol and tell me what it does) -
+#intergers = whole numbers
+#flouts = numbers with decimal
+#examples
+num1 = 3
+num2 = 7
+print("add (+)", num1 + num2)
+print("subtract (-)", num1 - num2)
+print("mutiply (*)", num1 * num2)
+print("divid (/)", num1 / num2)
+print("exponet (**)" ,num1 ** num2)
+print("interger division (//)", num1 // num2)
+print("modulo (%)", num1 % num2)
+
+#List ALL of the different assignment operators (Give me the symbols and what it does) -
+#
+
+#Why are expressions so important in programming-
+#
--- Notes/test.py
+++ Notes/test.py
@@ -1,5 +1,1 @@
-# LC 7th Github connection 
-
-age = input("what is your age")
-
-print("Hi :)",age)
+# LC 7th you test stuff here
