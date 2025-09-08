3389c16c7c50d917f66df06c8fdde6e887542081
Yourself77777 <lindon.corbin@ucas-edu.net>
2025-09-08 14:40:45 -0600

    commit

--- Notes/String_Notes.py
+++ Notes/String_Notes.py
@@ -1,3 +1,25 @@
 #LC 7th String Notes
 
-print("i did it")
+# What makes something a string-
+# -A set of symbles held together by quotation markes 
+#Examples
+first_name = input("What is your first name:\n").strip().title()
+
+last_name = input("what is your last name:\n").strip().title()
+
+full_name = first_name + " " + last_name
+sentence = '       the quick brown fox jumps over the lazy dog           ' 
+print(sentence.strip())
+
+print("welcome to my program", full_name + " ")
+
+# Why do we have strings
+# -
+
+# How do stupid proofing and sanitixation relate to each other-
+
+# What is debugging- 
+
+# How do you debug the different types of errors- 
+
+# Descrive what each of the methods below does-
