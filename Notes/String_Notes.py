c98374f7562826f4faab70e5a3ff2a10911808f3
Yourself77777 <lindon.corbin@ucas-edu.net>
2025-09-09 14:39:15 -0600

    finished string notes

--- Notes/String_Notes.py
+++ Notes/String_Notes.py
@@ -14,12 +14,39 @@ print(sentence.strip())
 print("welcome to my program", full_name + "!")
 
 # Why do we have strings
-# -
+# because they help keep people from breaking the code
 
 # How do stupid proofing and sanitixation relate to each other-
+# Sanitaxtion is the first step stupid proofing you code
 
 # What is debugging- 
+#getting rid of all problems 
 
 # How do you debug the different types of errors- 
+# types of bugs-
 
+# syntax Error- grammer and structur/ Example 
+# error "this is a bug" 
+# Fixed
+error = "this is a bug" 
+
+# Logic Error- is it ran the code but you get the wrong output/ Example
+numOne = "1"
+numTwo = "2"
+print(numOne + numTwo)
+# Fixed 
+numOne = 1
+numTwo = 2
+print(numOne + numTwo)
+
+# Run-Time Error- when it says nothing is wrong but it does not work/ Example
+num = 3
+# print("my favorite number is"+num)
+# Fixed
+print("my favorit number is", + num)
 # Descrive what each of the methods below does-
+find() # Tells us the begginning index of a line/ Example 
+print(sentence[sentence.find("lazy"): len (lazy)+sentence.find("lazy")])
+
+
+
