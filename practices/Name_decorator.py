8e354ed635c2f181cf92df06ac33b96301741c77
Yourself77777 <lindon.corbin@ucas-edu.net>
2025-09-10 14:22:22 -0600

    finished name decoratior

---
+++ practices/Name_decorator.py
@@ -0,0 +1,5 @@
+# LC 7th Name decorator
+
+name = input("what is your name:\n").capitalize()
+decoration = input("hello "+ name +" what do you want to decorate you name as:\n")
+print(decoration + name + decoration)
