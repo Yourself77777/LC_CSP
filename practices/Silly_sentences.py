f912e6cafd0f8737661ac3a3e75a54e725c96a6e
Yourself77777 <lindon.corbin@ucas-edu.net>
2025-09-10 14:38:13 -0600

    silly sentences

---
+++ practices/Silly_sentences.py
@@ -0,0 +1,11 @@
+# LC 7th Silly sentences
+
+word1 = input("Give me an verb:\n")
+word2 = input("Now give me a name:\n").capitalize()
+word3 = input("What is your favorite number:\n")
+word4 = input("what do you like to do in your free time:\n")
+
+print("The big dumb "+ word2 + " was jumping on the tramp.")
+print("When he did a "+ word1 + " and hit his head.")
+print("So he went inside and put "+ word3 + " bandades on it.")
+print("Then he started to play "+ word4 + " for a while.")
