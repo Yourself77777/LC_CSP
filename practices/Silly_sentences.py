77e13d1f39724bef92b7ca8fd16050894bb1d02b
Yourself77777 <lindon.corbin@ucas-edu.net>
2025-09-10 14:41:40 -0600

    commit code

--- practices/Silly_sentences.py
+++ practices/Silly_sentences.py
@@ -6,6 +6,6 @@ word3 = input("What is your favorite number:\n")
 word4 = input("what do you like to do in your free time:\n")
 
 print("The big dumb "+ word2 + " was jumping on the tramp.")
-print("When he did a "+ word1 + " and hit his head.")
+print("When s/he did a "+ word1 + " and hit his head.")
 print("So he went inside and put "+ word3 + " bandades on it.")
 print("Then he started to play "+ word4 + " for a while.")
