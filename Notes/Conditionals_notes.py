87d37f6425c196de872215e4bd06def11eee4bd4
Yourself77777 <lindon.corbin@ucas-edu.net>
2025-09-17 14:43:16 -0600

    notes

---
+++ Notes/Conditionals_notes.py
@@ -0,0 +1,53 @@
+# LC 7th Conditional notes
+
+# What puts something inside of the "if" statement?
+# if your are asking a question and want a specific statement for a specific answer
+
+# What do "if" statements do?
+# Tells code to check for true of false
+# Example
+#homework_done = input("is your homework done: ").strip().capitalize()
+
+#if homework_done == "Yes":
+    #print("Yes you can go!")
+
+
+# What are boolean statements?
+# They always show as a true or false
+
+# if not the answer for if statement then does this instead
+# Example
+# What do else statements do
+#else:
+    #print("Then go do your homework")
+
+# What kind of statement do you use if you have more than 2 needed outcomes?
+# Then do your least likly outcome first
+# Example 
+
+
+
+
+
+# What do each of the differents symbols mean in Conditionals?
+# Example 
+grade = input("What is your grade?")
+#<   / that is greater than
+if grade <= 89:
+    print(f"You have {grade}% that is an A!")
+elif grade <= 79:
+    print(f"You have {grade}% that is an B!")
+elif grade <= 70:
+    print(f"you have {grade}% that is a C!")
+else:
+    print(f"You have {grade}% that is a D or lower :(")
+
+
+
+# What are the 3 logical operatiors for?
+
+
+# What are logical operatiors for? 
+
+
+# What does a nested conditional statement do?
