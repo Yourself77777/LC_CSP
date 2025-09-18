5c881fd3223141a5d2f6dbf1fac6a0dd5203cb14
something <lindon.corbin@ucas-edu.net>
2025-09-18 21:57:56 +0000

    finished time of day

--- practices/time_of_day.py
+++ practices/time_of_day.py
@@ -1,2 +1,11 @@
 # LC 7th Time Of Day
 
+
+time = int(input("What is the time of day military time ther hour:\n"))
+
+if time >= 17:
+    print(f"Good evening it is {time}:00")
+elif time>= 12:
+    print(f"Good afternoon it is {time}:00")
+else:
+    print(f"good morning it is {time}:00")
