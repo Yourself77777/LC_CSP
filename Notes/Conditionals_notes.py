c1604136edc84311a5df49bb834ecadcf6c74bb4
Yourself77777 <lindon.corbin@ucas-edu.net>
2025-09-18 14:16:43 -0600

    finished Conditionl notes

--- Notes/Conditionals_notes.py
+++ Notes/Conditionals_notes.py
@@ -2,6 +2,7 @@
 
 # What puts something inside of the "if" statement?
 # if your are asking a question and want a specific statement for a specific answer
+# A.K.A. Tab
 
 # What do "if" statements do?
 # Tells code to check for true of false
@@ -22,32 +23,42 @@
     #print("Then go do your homework")
 
 # What kind of statement do you use if you have more than 2 needed outcomes?
-# Then do your least likly outcome first
-# Example 
-
-
-
-
+# Then do your least likly outcome first 
 
 # What do each of the differents symbols mean in Conditionals?
 # Example 
-grade = input("What is your grade?")
-#<   / that is greater than
-if grade <= 89:
-    print(f"You have {grade}% that is an A!")
-elif grade <= 79:
+grade = 110
+
+if grade >= 90:
+    if grade > 100:
+        print ("You cheated that isn't possible!")
+    else:
+        print(f"You have {grade}% that is an A!")
+elif grade >= 80:
     print(f"You have {grade}% that is an B!")
-elif grade <= 70:
+elif grade >= 70:
     print(f"you have {grade}% that is a C!")
 else:
     print(f"You have {grade}% that is a D or lower :(")
 
-
-
 # What are the 3 logical operatiors for?
+# and  
+# not  
+# or  
+# Example
+name = input("What is your name: ")
 
+if name == "Ms LaRose":
+    print("You are the teacher!")
+elif name == "Tia" or name == "Ashley":
+    print("You are the TA")
+else:
+    print(f"Hello {name}, you are a student!")
+    if grade >= 70:
+        print("And you are passing the class!")
 
 # What are logical operatiors for? 
-
+# so you can meet all the oporations that you need
 
 # What does a nested conditional statement do?
+# a condition that only happens inside anothor one
