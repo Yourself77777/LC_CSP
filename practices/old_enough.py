889d8b178803092aa6d11eebab8e0ca24a1eaa5f
Yourself77777 <lindon.corbin@ucas-edu.net>
2025-09-18 14:40:05 -0600

    old enough done

---
+++ practices/old_enough.py
@@ -0,0 +1,15 @@
+# LC 7th Old Enough
+
+age = int(input("How old are you\n"))
+
+
+if age >= 18:
+    print("Go vote")
+elif age >= 16:
+    print("Go drive without an adult")
+elif age >= 15:
+    print("Go get a learner permit")
+else:
+    print("Go to school")
+
+
---
+++ practices/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+# LC 7th Old Enough
+
+age = int(input("How old are you"))
+
+if age >= 18:
+    print("You can vote!")
