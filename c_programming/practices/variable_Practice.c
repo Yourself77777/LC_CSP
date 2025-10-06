b52e92273fea23fab6acde2de304953eae4107c5
yourself77777 <lindon.corbin@ucas-edu.net>
2025-10-06 14:41:59 -0600

    started variable

---
+++ c_programming/practices/variable_Practice.c
@@ -0,0 +1,20 @@
+// LC 7th Variable Practice
+#include <stdio.h>
+
+int main(void){
+
+    char name = "Lindon";
+    int lucky_number = 3;
+    float decimal_number = 676.9;
+    char breakfast = "Eggs with toast";
+    char favorite_color = "Navy Blue";
+    char school_name = "UCAS";
+    int current_year = 2025;
+    char eye_color = "Brown";
+    int age = 14;
+    char favorite_subject = "Chemisty";
+
+    printf
+    return 0;
+}
+
