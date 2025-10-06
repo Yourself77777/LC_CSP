7b716e7165094ba2485c7a5cbcf6eb7c991cf87d
yourself77777 <lindon.corbin@ucas-edu.net>
2025-10-06 15:55:26 -0600

    did variable practice

--- c_programming/practices/variable_Practice.c
+++ c_programming/practices/variable_Practice.c
@@ -3,18 +3,29 @@
 
 int main(void){
 
-    char name = "Lindon";
+    char name[] = "Lindon";
     int lucky_number = 3;
     float decimal_number = 676.9;
-    char breakfast = "Eggs with toast";
-    char favorite_color = "Navy Blue";
-    char school_name = "UCAS";
+    char breakfast[] = "Eggs with toast";
+    char favorite_color[] = "Navy Blue";
+    char school_name[] = "UCAS";
     int current_year = 2025;
-    char eye_color = "Brown";
+    char eye_color[] = "Brown";
     int age = 14;
-    char favorite_subject = "Chemisty";
+    char favorite_subject[] = "Chemisty";
 
-    printf
+    printf("My name is %s\n", name);
+    printf("My lucky number is %d\n", lucky_number);
+    printf("A decimal number example is %f\n", decimal_number);
+    printf("What is had for breakfast was %s\n", breakfast);
+    printf("My favorte color is %s\n", favorite_color);
+    printf("I go to %s\n", school_name);
+    printf("The current year is %d\n", current_year);
+    printf("My eye color is %s\n", eye_color);
+    printf("I am %d years old\n", age);
+    printf("My favorte subject is %s\n", favorite_subject);
+
+
     return 0;
 }
 
